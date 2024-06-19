ae02a214928ab9f8f8b64f50b2b636d00d98f4eb
Cvet-34 <167579626+Cvet-34@users.noreply.github.com>
2024-06-19 15:30:51 +0500

    Add files via upload

---
+++ доработка ДЗ Атрибуты и методы объекта..py
@@ -0,0 +1,19 @@
+class House: # Создали класс House.
+    def __init__(self, name, number_of_floors): # создали метод __init__ создали отребуты
+        self.name = name    # self = указание на обект
+        self.number_of_floors = number_of_floors
+
+    def go_to(self):
+        self.new_floor  = int(input('нажмите кнопку этажа для подъема ')) # пользователь задает № этажа
+        if self.number_of_floors >= self.new_floor >= 1: # если № этажа в диапозоне от 1-30 то "Поехали"
+            for i in range(1, self.new_floor + 1):  # Выводит в консоль этажи с 1 до указанного пользователем.
+                print(i)
+        elif self.number_of_floors <= self.new_floor or self.new_floor < 1: # если № этажа в диапозоне до 0 или больше 30 то "нет такого этажа"
+            print('Такого этажа несуществует')
+
+
+
+
+gk = House('ЖК Эльбрус', 30) # передали значения атрибутов
+print(gk.name, gk.number_of_floors)
+gk.go_to()   # Применили логику прописанную в функции для нашей переменной-объекта.
