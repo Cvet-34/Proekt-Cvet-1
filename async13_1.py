193af8561663fc467bd0483ed0f2d1635357a93d
Cvet-34 <167579626+Cvet-34@users.noreply.github.com>
2025-01-11 20:53:48 +0500

    Add files via upload

---
+++ async13_1.py
@@ -0,0 +1,23 @@
+import asyncio
+
+#name - имя силача
+#power - подъёмная мощность
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования')
+    for i in range(5):
+        await asyncio.sleep(1 / power)
+        print(f'Силач {name} поднял {i + 1} шар')
+    print(f'Силач {name} закончил соревнования.')
+
+#Начало соревнований по поднятию шаров Атласа'''
+async def start_tournament():
+    task1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task2 = asyncio.create_task(start_strongman('Denis', 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task1
+    await task2
+    await task3
+
+
+if __name__ == '__main__':
+    asyncio.run(start_tournament()) # asyncio.run запуск работы фукции
